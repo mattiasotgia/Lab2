ce4211891b3cd5717ee35f5cf18f3b53dc7b4d76
Francesco Polleri <91823936+francescopolleri@users.noreply.github.com>
2021-11-30 12:54:35 +0100

    .

--- esperienza_4__irraggiamento_lampadina/analisi_dati/guadagno.C
+++ esperienza_4__irraggiamento_lampadina/analisi_dati/guadagno.C
@@ -1,65 +1,64 @@
-#include<vector>
-#include<cmath>
-#include<iostream>
-#include<fstream>
-#include<string>
+#include <vector>
+#include <cmath>
+#include <iostream>
+#include <fstream>
+#include <string>
 
-#include<TCanvas.h>
-#include<TGraphErrors.h>
-#include<TF1.h>
-#include<TStyle.h>
-#include<TAxis.h>
-#include<TMath.h>
-#include<TLatex.h>
-#include<TLegend.h>
+#include <TCanvas.h>
+#include <TGraphErrors.h>
+#include <TF1.h>
+#include <TStyle.h>
+#include <TAxis.h>
+#include <TMath.h>
+#include <TLatex.h>
+#include <TLegend.h>
 
-#include"../../LabTools/LabTools.h"
+#include "../../LabTools/LabTools.h"
 
-double get_VRangeErr(double errPercent, int partitions, double range1){return errPercent * partitions *  range1;}
+double get_VRangeErr(double errPercent, int partitions, double range1) { return errPercent * partitions * range1; }
 
-void guadagno(){
-    string nomefile;    
+void guadagno()
+{
 
-std::cout<<"nome file input"<<std::endl;
-std::cin>>nomefile>>std::endl;
+    string nomefile;
 
-double vin, vout, fsvin, fsvout;
-const double errPercent=0.035;
+    std::cout << "nome file input" << std::endl;
+    std::cin >> nomefile >> std::endl;
 
-TGraphErrors g;
-TF1 f ("fit","[0]+[1]*x");
-int i=0;
+    double vin, vout, fsvin, fsvout;
+    const double errPercent = 0.035;
 
-while (file>>vin>>fsvin>>vout>>fsvout){
-double err_vin=get_VRangeErr(errPercent, 8, fsvin);
-double err_vout=get_VRangeErr(errPercent,8,fsvout);
+    TGraphErrors*  g=new TGraphErrors();
+    TF1* f= new TF1("fit", "[0]+[1]*x");
+    int i = 0;
 
-g.SetPoint(i,vin,vout);
-g.SetPointError(i,err_vin,err_vout);
-i++;
+    while (file >> vin >> fsvin >> vout >> fsvout)
+    {
+        double err_vin = get_VRangeErr(errPercent, 8, fsvin);
+        double err_vout = get_VRangeErr(errPercent, 8, fsvout);
 
-}
+        g->SetPoint(i, vin, vout);
+        g->SetPointError(i, err_vin, err_vout);
+        i++;
+    }
 
-g.Draw("ap");
-f.SetParameter(0,0);
-f.SetParameter(1,80);
+    g->Draw("ap");
+    f->SetParameter(0, 0);
+    f->SetParameter(1, 80);
 
-g.Fit("fit");
+    g->Fit("fit");
 
-double G = f.GetParameter(1);
-double quota = f.GetParameter(0);
+    double G = f_>GetParameter(1);
+    double quota = f.GetParameter(0);
 
-double e_quota = f.GetParError(0);
-double e_G = f.GetParError(1);
+    double e_quota = f->GetParError(0);
+    double e_G = f->GetParError(1);
 
-double chi2 = f.GetChisquare();
-double prob = f.GetProb();
-double ndf = f.GetNDF();
+    double chi2 = f->GetChisquare();
+    double prob = f->GetProb();
+    double ndf = f->GetNDF();
 
-std::cout<<"il valore del guadagno è:"<<G<<"+/-"<<e_G<<std::endl;
-std::cout<<"il valore della quota è:"<<quota<<"+/-"<<e_quota<<std::endl;
-
-
-
-
+    std::cout << "il valore del guadagno è:" << G << "+/-" << e_G << std::endl;
+    std::cout << "il valore della quota è:" << quota << "+/-" << e_quota << std::endl;
+    log::print_stat(f);
 }
