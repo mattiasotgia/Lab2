141105cbdc8e8180ea20081830c3bbe9782bc040
Francesco Polleri <91823936+francescopolleri@users.noreply.github.com>
2021-11-30 12:48:40 +0100

    .

--- esperienza_4__irraggiamento_lampadina/analisi_dati/guadagno.C
+++ esperienza_4__irraggiamento_lampadina/analisi_dati/guadagno.C
@@ -21,12 +21,13 @@ void guadagno(){
     string nomefile;    
 
 std::cout<<"nome file input"<<std::endl;
-std::cin>>nomefile>>endl;
+std::cin>>nomefile>>std::endl;
 
 double vin, vout, fsvin, fsvout;
-double errPercent=0.035;
+const double errPercent=0.035;
 
 TGraphErrors g;
+TF1 f ("fit","[0]+[1]*x");
 int i=0;
 
 while (file>>vin>>fsvin>>vout>>fsvout){
@@ -38,5 +39,27 @@ g.SetPointError(i,err_vin,err_vout);
 i++;
 
 }
+
+g.Draw("ap");
+f.SetParameter(0,0);
+f.SetParameter(1,80);
+
+g.Fit("fit");
+
+double G = f.GetParameter(1);
+double quota = f.GetParameter(0);
+
+double e_quota = f.GetParError(0);
+double e_G = f.GetParError(1);
+
+double chi2 = f.GetChisquare();
+double prob = f.GetProb();
+double ndf = f.GetNDF();
+
+std::cout<<"il valore del guadagno è:"<<G<<"+/-"<<e_G<<std::endl;
+std::cout<<"il valore della quota è:"<<quota<<"+/-"<<e_quota<<std::endl;
+
+
+
 
 }
