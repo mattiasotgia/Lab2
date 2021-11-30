4d5286b06a02293f452767fbd236aaa5e6270a7c
Mattia Sotgia <21292823+mattiasotgia@users.noreply.github.com>
2021-11-30 16:16:22 +0100

    fixed cin

--- esperienza_4__irraggiamento_lampadina/analisi_dati/analisi_BODE.C
+++ esperienza_4__irraggiamento_lampadina/analisi_dati/analisi_BODE.C
@@ -33,7 +33,7 @@ void analisi_BODE(double fitmin = -1){
 
     std::string file = "eliminare_test.txt";
     std::cout << "File input : " << std::flush;
-    std::cin << file;
+    std::cin >> file;
     std::string rawdata = "../dati/" + file;
     std::string output = file.substr(0, file.find("."));
     std::ifstream data(rawdata.c_str());
