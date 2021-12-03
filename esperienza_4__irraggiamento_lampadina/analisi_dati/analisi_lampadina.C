5424fdc9025cb32a83e456d0de2f2c2d64dac05a
Francesco Polleri <91823936+francescopolleri@users.noreply.github.com>
2021-12-03 09:15:38 +0100

    .

--- esperienza_4__irraggiamento_lampadina/analisi_dati/analisi_lampadina.C
+++ esperienza_4__irraggiamento_lampadina/analisi_dati/analisi_lampadina.C
@@ -30,17 +30,24 @@ void analisi_lampadina()
    // const double errPercent = 0.035;
 
    TGraphErrors*  g=new TGraphErrors();
-    //TF1* f= new TF1("fit", "[0]+[1]*x");
+ //TF1* f= new TF1("fit", "[0]+[1]*x");
+
     int i = 0;
 
     ifstream file(("../dati/" + nomefile).c_str());
+
+   /* if(! file.good() ){
+        std::cout<<"errore lettura file"<<std::endl;
+   */ }
 
    while (file >> t >> vin >> e_vin >> range){
 
         g->SetPoint(i, t, vin);
         g->SetPointError(i, 0, e_vin);
         i++;
+
     }
+
 g->Draw("ap");
 
 g->SetTitle("V_in vs t;tempo[s];V_in[V]");
