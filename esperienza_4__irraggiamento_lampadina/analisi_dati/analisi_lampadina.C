c6dda2f014dab5a07714e5318746ca5fe02ac6a6
Francesco Polleri <91823936+francescopolleri@users.noreply.github.com>
2021-12-03 11:05:46 +0100

    .

--- esperienza_4__irraggiamento_lampadina/analisi_dati/analisi_lampadina.C
+++ esperienza_4__irraggiamento_lampadina/analisi_dati/analisi_lampadina.C
@@ -25,31 +25,30 @@ void analisi_lampadina()
     std::cout << "nome file input" << std::endl;
     std::cin >> nomefile;
 
-    double t , vin, e_vin, range;
-    const double s_vin=0.01;
-   // const double errPercent = 0.035;
+    double t, vin, e_vin, range;
+    const double s_vin = 0.01;
+    // const double errPercent = 0.035;
 
-   TGraphErrors*  g=new TGraphErrors();
- //TF1* f= new TF1("fit", "[0]+[1]*x");
+    TGraphErrors *g = new TGraphErrors();
+    //TF1* f= new TF1("fit", "[0]+[1]*x");
 
     int i = 0;
 
     ifstream file(("../dati/" + nomefile).c_str());
 
-   /* if(! file.good() ){
+    /* if(! file.good() ){
         std::cout<<"errore lettura file"<<std::endl;
-   */ }
+   */
 
-   while (file >> t >> vin >> e_vin >> range){
+    while (file >> t >> vin >> e_vin >> range)
+    {
 
         g->SetPoint(i, t, vin);
         g->SetPointError(i, 0, e_vin);
         i++;
-
     }
 
-g->Draw("ap");
+    g->Draw("ap");
 
-g->SetTitle("V_in vs t;tempo[s];V_in[V]");
-
+    g->SetTitle("V_in vs t;tempo[s];V_in[V]");
 }
