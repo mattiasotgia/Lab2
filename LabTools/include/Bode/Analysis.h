33dcf387a5e8a3e8b71de7a6f319b8d77c7bf2be
Mattia Sotgia <21292823+mattiasotgia@users.noreply.github.com>
2022-02-01 11:03:02 +0100

    Added Mathods, Bode class

--- LabTools/include/Bode/Analysis.h
+++ LabTools/include/Bode/Analysis.h
@@ -1,29 +1,60 @@
+/**
+ * @file Analysis.h
+ * @author Mattia Sotgia (mattiasotgia01@gmail.com)
+ * @brief 
+ * @version 0.1
+ * @date 2022-02-01
+ * 
+ * @copyright Copyright (c) 2022
+ * 
+ */
+
 #ifndef LABTOOLS_BODE_Analysis
 #define LABTOOLS_BODE_Analysis
 
 #include<iostream>
-#include<TGraphErrors.h>
+#include<vector>
+
+#include<TCanvas.h>
 #include<TF1.h>
+#include<TGraphErrors.h>
 
 class Bode{
 private:
-    bool                _isfunctioncalled; //> check if user called for function different from base;
+    bool                _isfunctioncalled = false; ///> check if user called for function different from base;
 
     const char         *_gainfit = ""; // todo: fill function
     const char         *_phasefit = ""; // todo: fill function
 
-    Int_t               fmin = (0.0); //> minimum for frequency range
-    Int_t               fmax = (1.0); //> maximum for frequency range
-
+    /// graphical objects []
+    TCanvas            *fFigure;
     TGraphErrors       *fGain;
     TGraphErrors       *fPhase;
     TF1                *fGainFit; 
     TF1                *fPhaseFit;
+
+    /// function variables declaration
+    Double_t            fmin = (0.0);   ///> minimum for frequency range
+    Double_t            fmax = (1.0);   ///> maximum for frequency range
+    Double_t           *fPointGain;     ///>[fNpoints] array for gain points
+    Double_t           *fPErrGain;      ///>[fNpoints] array for error gain points
+    Double_t           *fPointPhase;    ///>[fNpoints] array for phase points
+    Double_t           *fPErrPhase;     ///>[fNpoints] array for error phase points
 public:
     Bode();
+    Bode(const char *filename, Option_t *option="");
     ~Bode();
-    inline void         SetLogx();
-    inline void         SetLogy();
+    void                FitCorrelated(Option_t *option="", Option_t *goption="", Axis_t xmin=0, Axis_t xmax=0);
+    bool                ReadInput(const char *filename, Option_t *option="");       ///> read input for both phase and gain data 
+    bool                ReadInputGain(const char *filename, Option_t *option="");   ///> read input for gain data
+    bool                ReadInputPhase(const char *filename, Option_t *option="");  ///> read input for phase data
+    // bool                ReadInputRDF()  // TO BE IMPLEMENTED
+    void                SetGainFunction(const char *formula, Option_t *option="");
+    bool                SetGainVec(std::vector<Double_t> Gain, std::vector<Double_t> ErrGain);
+    void                SetPhaseFunction(const char *formula, Option_t *option="");
+    bool                SetPhaseVec(std::vector<Double_t> Phase, std::vector<Double_t> ErrPhase);
+    void                SetLogx();
+    void                SetLogy();
 };
 
 #endif
--- LabTools/src/Bode/Analysis.cpp
+++ LabTools/src/Bode/Analysis.cpp
@@ -1,13 +1,66 @@
+/**
+ * @file Analysis.cpp
+ * @author Mattia Sotgia (mattiasotgia01@gmail.com)
+ * @brief 
+ * @version 0.1
+ * @date 2022-02-01
+ * 
+ * @copyright Copyright (c) 2022
+ * 
+ */
 
 #include<iostream>
 
+#include<TCanvas.h>
+#include<TF1.h>
+#include<TGraphErrors.h>
 
 #include"Bode/Analysis.h"
 #include"LabPlot.h" // set_atlas_style() called from here
+#include"Logger.h"
 
 Bode::Bode(){
+    set_atlas_style()
     fGain = new TGraphErrors();
     fPhase = new TGraphErrors();
     fGainFit = new TF1("gainfit", _gainfit, fmin, fmax);
     fPhaseFit = new TF1("phasefit", _phasefit, fmin, fmax);
+}
+
+bool Bode::SetGainVec(std::vector<Double_t> Gain, std::vector<Double_t> ErrGain){
+    fPointGain = Gain.data(); 
+    fPErrGain = ErrGain.data();
+
+    if (Gain.size()!=ErrGain.size()){
+        printf("%s", Logger::error("array size of Gain and its Error do not match!"));
+        return false;
+    }
+
+    Int_t kSizePG = sizeof(fPointGain)/sizeof(Double_t);
+    Int_t kSizeEPG = sizeof(fPErrGain)/sizeof(Double_t);
+
+    if(Gain.size()!=kSizePG || ErrGain.size()!=kSizeEPG){
+        printf("%s", Logger::error(Form("size of Gain input vec: %s, size of Gain copy vec: %s", Gain.size(), kSizePG)));
+        printf("%s", Logger::error(Form("size of ErrGain input vec: %s, size of ErrGain copy vec: %s", Gain.size(), kSizePG)));
+        return false;
+    }
+}
+
+bool Bode::SetPhaseVec(std::vector<Double_t> Phase, std::vector<Double_t> ErrPhase){
+    fPointPhase = Phase.data(); 
+    fPErrPhase = ErrPhase.data();
+
+    if (Phase.size()!=ErrPhase.size()){
+        printf("%s", Logger::error("array size of Phase and its Error do not match!"));
+        return false;
+    }
+
+    Int_t kSizePG = sizeof(fPointPhase)/sizeof(Double_t);
+    Int_t kSizeEPG = sizeof(fPErrPhase)/sizeof(Double_t);
+
+    if(Phase.size()!=kSizePG || ErrPhase.size()!=kSizeEPG){
+        printf("%s", Logger::error(Form("size of Phase input vec: %s, size of Phase copy vec: %s", Phase.size(), kSizePG)));
+        printf("%s", Logger::error(Form("size of ErrPhase input vec: %s, size of ErrPhase copy vec: %s", Phase.size(), kSizePG)));
+        return false;
+    }
 }
