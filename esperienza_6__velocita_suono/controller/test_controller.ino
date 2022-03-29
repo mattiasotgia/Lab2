f8090a0c3f8c1bee88df77d495d2d7a3b34964da
Mattia Sotgia <21292823+mattiasotgia@users.noreply.github.com>
2022-03-29 17:44:58 +0200

    data && ino testing

---
+++ esperienza_6__velocita_suono/controller/test_controller.ino
@@ -0,0 +1,22 @@
+/**
+ * @file test_controller.ino
+ * @author Mattia Sotgia 
+ * @brief testing 
+ * @version 0.1
+ * @date 2022-03-29
+ * 
+ * @copyright Copyright (c) 2022
+ * 
+ */
+
+int led = LED_BUILTIN;
+volatile byte ledStatus = LOW;
+
+void setup(){
+    Serial.begin(9600);
+    pinMode(led, OUTPUT);
+}
+
+void loop(){
+    digitalWrite()
+}
