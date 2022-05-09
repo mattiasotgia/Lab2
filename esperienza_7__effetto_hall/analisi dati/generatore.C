7f4bf01ca05fb6ea357e6392eadee7bec61957e7
Francesco Polleri <91823936+francescopolleri@users.noreply.github.com>
2022-05-09 17:15:17 +0200

    programmi per guadagno e generatore

---
+++ esperienza_7__effetto_hall/analisi dati/generatore.C
@@ -0,0 +1,76 @@
+#include <vector>
+#include <cmath>
+#include <iostream>
+#include <fstream>
+#include <string>
+
+#include <TCanvas.h>
+#include <TGraphErrors.h>
+#include <TF1.h>
+#include <TStyle.h>
+#include <TAxis.h>
+#include <TMath.h>
+#include <TLatex.h>
+#include <TLegend.h>
+
+#include "LabTools.h"
+
+
+double get_VRangeErr(double errPercent, int partitions, double range1) { return errPercent * partitions * range1; }
+
+double get_IRangeErr(double meas) {return meas*0.00001*500+0.00001*400*0.1;} //verificare se la misura della corrente è in A o mA
+
+void generatore(){
+
+ifstream file(("../dati/generatore.txt").c_str());
+
+const double errPercent = 0.035;
+
+double V, meas, range1;
+
+TCanvas* c1 = new TCanvas("", "", 600, 500);
+TGraphErrors* g=new TGraphErrors();
+TF1* f=newTF1("f","[0]+[1]*x");
+
+int i=0;
+
+while(file>>V>>range1>>meas){
+
+g->SetPoint(i,V,meas);
+g->SetPointError(i,get_VRangeErr(errPercent,8,range1),get_IRangeErr(meas));
+
+i++;
+
+}
+
+g->Draw("ap");
+
+f->SetParameters(0,1/500);
+
+g->Fit("f");
+
+f->SetParName(0,"quota");
+f->SetParName(1,"k");
+
+double k = f->GetParameter(1);
+double quota = f->GetParameter(0);
+double e_quota = f->GetParError(0);
+double e_k = f->GetParError(1);
+
+double chi2 = f->GetChisquare();
+double prob = f->GetProb();
+double ndf = f->GetNDF();
+
+std::cout << "il valore del coefficiente k è: " << k << " +- " << e_k << std::endl;
+std::cout << "il valore della quota è: " << quota << " +- " << e_quota << std::endl;
+logs::print_stat(f);
+
+c1->SaveAs(("../fig/generatore.pdf").c_str());
+
+
+
+
+
+
+
+}
---
+++ esperienza_7__effetto_hall/analisi dati/guadagno.C
@@ -0,0 +1,87 @@
+#include <vector>
+#include <cmath>
+#include <iostream>
+#include <fstream>
+#include <string>
+
+#include <TCanvas.h>
+#include <TGraphErrors.h>
+#include <TF1.h>
+#include <TStyle.h>
+#include <TAxis.h>
+#include <TMath.h>
+#include <TLatex.h>
+#include <TLegend.h>
+
+#include "LabTools.h"
+
+double get_VRangeErr(double errPercent, int partitions, double range1) { return errPercent * partitions * range1; }
+
+void guadagno()
+{
+
+    graphset::init();
+    gStyle->SetFrameLineWidth(0);
+
+    std::string nomefile;
+
+    std::cout << "nome file input" << std::endl;
+    std::cin >> nomefile;
+    std::string output = nomefile.substr(nomefile.find_last_of("_"), nomefile.find(".")-nomefile.find_last_of("_"));
+
+    double vin, vout, fsvin, fsvout;
+    const double errPercent = 0.035;
+    TCanvas* c1 = new TCanvas("", "", 600, 500);
+    graphset::setcanvas(c1);
+
+    TGraphErrors*  g=new TGraphErrors();
+    TF1* f= new TF1("fit", "[0]+[1]*x");
+    int i = 0;
+
+    ifstream file(("../dati/" + nomefile).c_str());
+
+    while (file >> vin >> fsvin >> vout >> fsvout)
+    {
+        double err_vin = get_VRangeErr(errPercent, 8, fsvin);
+        double err_vout = get_VRangeErr(errPercent, 8, fsvout);
+
+        g->SetPoint(i, vin, vout);
+        g->SetPointError(i, err_vin, err_vout
+        i++;
+    }
+
+    g->Draw("ap");
+    f->SetParameter(0, 0);
+    f->SetParameter(1, 80);
+    f->SetParName(0,"quota");
+    f->SetParName(1,"G");
+
+    g->Fit("fit");
+
+    std::cout << Form("%s \n", output.c_str());
+
+    TLatex* text = new TLatex();
+    text->SetTextFont(43);
+    text->SetTextSize(20);
+    text->DrawLatexNDC(0.20, 0.85, (Form("#splitline{#bf{GUADAGNO} Configurazione %s}{#chi^{2}/ndf (prob.) = %.2f/%d (%.3f)}", (output=="_invertente"? "Invertente":"Non-Invertente"), f->GetChisquare(), f->GetNDF(), f->GetProb())));
+
+
+
+    double G = f->GetParameter(1);
+    double quota = f->GetParameter(0);
+
+    double e_quota = f->GetParError(0);
+    double e_G = f->GetParError(1);
+
+    double chi2 = f->GetChisquare();
+    double prob = f->GetProb();
+    double ndf = f->GetNDF();
+
+    graphset::set_TGraphAxis(g, "Tensione uscita V_{out} [V]", 1.5, "Tensione ingresso V_{in} [V]");
+
+    std::cout << "il valore del guadagno è: " << G << " +- " << e_G << std::endl;
+    std::cout << "il valore della quota è: " << quota << " +- " << e_quota << std::endl;
+    logs::print_stat(f);
+
+    c1->SaveAs(("../fig/Guadagno_" + output + ".pdf").c_str());
+}
