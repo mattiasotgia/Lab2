7333506dca8b6913223b8da315f3eaf5a444bbd2
Mattia Sotgia <21292823+mattiasotgia@users.noreply.github.com>
2022-05-12 19:31:28 +0200

    cose a caso

--- esperienza_7__effetto_hall/controller/SYSTEM_main/SYSTEM_main.ino
+++ esperienza_7__effetto_hall/controller/SYSTEM_main/SYSTEM_main.ino
@@ -9,39 +9,63 @@
  * 
  */
 
-#define A0def   A0  // analog pin to read data form op-amp/strum V_H
+#define A0def   A10  // analog pin to read data form op-amp/strum V_H
 #define D5V_pin 2   // digital pin: 5V write if masure w/ i, =V otherwise
-#define A1def   A1  // analog pin to send V_x (reference for converter)
+#define A3def   A13  // analog pin to send V_x (reference for converter)
 
-void serial3_sendmmsg_delay(const char* fmt){
-    Serial3.print(fmt);
-    delay(100);
+void Serial2_sendmmsg_delay(const char* fmt){
+    Serial2.println(fmt);
+    delay(1000);
     return;
 }
 
-void data_routine() 
+void serial_writeboth(double V_gen, double V_out){
+    Serial.print(V_out, 20);
+    Serial.print("\n");
+    Serial.print(V_gen, 20);
+    Serial.print("\n");
+};
+
+void data_routine(const char* step) 
 {
 
-    int M = 13;
+    int M = 12;
     int N = 100;
+
+    double V_gen, V_out;
 
     for ( int i=0; i<M; i++ ) {
 
+        Serial.print("*CYCLESTART\n");
+        Serial.print("I:");
+        Serial.print((float)i/10);
+        Serial.print("\n");
+
         for ( int j=0; j<N; j++ ) {
             digitalWrite(D5V_pin, HIGH);
-            delay(100);
+            delay(50);
             // eseguire la misura di A0
+            V_out = (double)((double)analogRead(A0def)*5.0/(double)1023);
             // eseguire la misura di A3
+            V_gen = (double)((double)analogRead(A3def)*5.0/(double)1023);
             // scrivere tutto sul seriale
-            delay(100);
+            serial_writeboth(V_out, V_gen);
+
+            delay(50);
             digitalWrite(D5V_pin, LOW);
-            delay(100);
+            delay(50);
             // eseguire la misura di A0
+            V_out = (double)((double)analogRead(A0def)*5.0/(double)1023);
             // eseguire la misura di A3
+            V_gen = (double)((double)analogRead(A3def)*5.0/(double)1023);
             // scrivere tutto sul seriale
-            delay(100);
+            serial_writeboth(V_out, V_gen);
+
+            delay(50);
         }
-        serial3_sendmmsg_delay("INCI1");
+        Serial2_sendmmsg_delay(step);
+        Serial.print("CYCLEEND*\n");
+        delay(100);
     }
     return;
 }
@@ -51,37 +75,43 @@ int main(void)
 {
     init();
 
-    Serial3.begin(9600);
+    Serial2.begin(9600);
     Serial.begin(9600);
 
     pinMode(D5V_pin, OUTPUT);
     pinMode(A0def, INPUT);
-    pinMode(A1def, OUTPUT);
+    pinMode(A3def, INPUT);
 
-    serial3_sendmmsg_delay("V1 30\n");
-    serial3_sendmmsg_delay("I1 0\n");
-    serial3_sendmmsg_delay("OP1 1\n");
-    serial3_sendmmsg_delay("DELTAI1 1e-1\n");
+    Serial2_sendmmsg_delay("V1 30");
+    Serial2_sendmmsg_delay("I1 1.3");
+    Serial2_sendmmsg_delay("OP1 1");
+    Serial2_sendmmsg_delay("DELTAI1 1e-1");
+    // analogReadResolution(12);
 
-    // char buffer[40];
+    // while ( true ) {
 
-    while ( true ) {
-        if ( Serial.available() > 0 ){
-            if ( strcmp((Serial.readString()).c_str(), "?STR") == 0 ) {
+    delay(5000);
+    Serial.print("*RUN\n");
+    Serial.print("*BPOS\n");
+    delay(100);
+    
+    data_routine("DECI1");
+    Serial.print("Change polarization in B and in OPAMP input! (45s left)\n");
+    delay(45000);
+    // wait to change polarization for B
+    
 
-                delay(100);
-                Serial.print("*RUN\n");
-                delay(100);
-                
-                data_routine();
-                
-                serial3_sendmmsg_delay("V1 0\n");
-                serial3_sendmmsg_delay("I1 0\n");
-                serial3_sendmmsg_delay("OP1 0\n");
-                Serial.print("*STP\n");
-            }
-        }
-    }
+    Serial.print("*BPOS\n");
+    delay(100);
+    data_routine("INCI1");
+            
+    Serial2_sendmmsg_delay("V1 0");
+    Serial2_sendmmsg_delay("I1 0");
+    Serial2_sendmmsg_delay("OP1 0");
+    // Serial.print("*STP\n");
+        // Serial.
+        // break;
+    // }
 
     return 0;
 }
--- esperienza_7__effetto_hall/controller/serialREADER/hall_serializer/main.py
+++ esperienza_7__effetto_hall/controller/serialREADER/hall_serializer/main.py
@@ -22,12 +22,7 @@ BASE_DATA_PATH = '/Users/massimosotgia/Desktop/uni_at_DIFI/Lab2/esperienza_7__ef
 
 def setserial(port, baudrate = 9600):
     devSERIAL = Serial(port=port, baudrate=baudrate)
-    try:
-        devSERIAL.open() 
-    except SerialException:
-        print('Unable to establish serial comm. on port {}'.format(devSERIAL.port))
-    except:
-        print('Unknown error!')
+    return devSERIAL
 
 def serial_write(serial: Serial, command):
     '''
@@ -35,11 +30,7 @@ def serial_write(serial: Serial, command):
         - `serial`: serial port
         - `command`: command in string or char array
     '''
-    
-    try:
-        serial.write('{}\n'.format(command.encode('utf-8')))
-    except:
-        print('Failed command "{} to serial {serial}"'.format(command, serial=serial.port))
+    serial.write(bytes(f'{command}\n', encoding='utf-8'))
     time.sleep(0.1)
 
 
@@ -48,17 +39,81 @@ def main():
     
     Bpol = ['Bpos', 'Bneg', 'B?']
     today: str = datetime.today().strftime('%Y%m%d_%H.%M.%S.%f');
-    
-    if ( len(sys.argv) > 1):
-        if (sys.argv[1] == '0' or sys.argv[1] == '1'):
-            output_file: str = 'dataRUN_{}_{}_HALL.root'.format(Bpol[int(sys.argv[1])], today)
-    else:
-        output_file: str = 'dataRUN_B?_{}_HALL.root'.format(today)
+    output_file: str = 'dataRUN_{}_HALL.root'.format(today)
+
     
     _file0 = root.TFile(join(BASE_DATA_PATH, output_file), 'RECREATE')
 
-    _data0ttree = root.TTree('data_{}'.format(Bpol[int(sys.argv[1]) if len(sys.argv) > 1 else 2]), 'RUN data E7/hall')
     
 
+    # root.gInterpreter.ProcessLine('''
+    #     struct Event {
+    #         int ID;
+    #         int RUN;
+    #         double V_gen_lo[12][100];
+    #         double V_gen_hi[12][100];
+    #         double V_out_lo[12][100];
+    #         double V_out_hi[12][100];
+    #     };
+    # ''')
+
+    devSERIAL = setserial(port='/dev/tty.usbmodem141401')
+
+    M = 12
+    N = 100
+
+    while(True):
+        if(devSERIAL.readline().decode('utf-8') == '*RUN\n'):
+            print('serial comm. to START data aquisition\n')
+            line = devSERIAL.readline().decode('utf-8')
+            if(line == '*BPOS'):
+                _data0ttreeBpos = root.TTree('data_BPOS', 'RUN data E7/hall: positive B')
+                _data0ttreeBpos.Branch('event', root.Event())
+                for i in range(M):
+                    if(devSERIAL.readline().decode('utf-8')=='*CYCLESTART'):
+                        pass
+                    print(devSERIAL.readline().decode('utf-8'))
+                    for j in range(N):
+                        root.Event.ID = 0 # Bpos
+                        root.Event.RUN = i
+
+                        V_out_hi[i][j] = float(devSERIAL.readline().decode('utf-8'))
+                        print(V_out_hi[i][j])
+                        V_gen_hi[i][j] = float(devSERIAL.readline().decode('utf-8'))
+                        print(V_gen_hi[i][j])
+                        V_out_lo[i][j] = float(devSERIAL.readline().decode('utf-8'))
+                        print(V_out_lo[i][j])
+                        V_gen_lo[i][j] = float(devSERIAL.readline().decode('utf-8'))
+                        print(V_gen_lo[i][j])
+                    print(devSERIAL.readline().decode('utf-8'))
+                    _data0ttreeBpos.Fill()
+            else:
+                _data0ttreeBneg = root.TTree('data_BNEG', 'RUN data E7/hall: negative B')
+                _data0ttreeBneg.Branch('event', root.Event())
+                for i in range(M):
+                    if(devSERIAL.readline().decode('utf-8')=='*CYCLESTART'):
+                        pass
+                    print(devSERIAL.readline().decode('utf-8'))
+                    for j in range(N):
+                        root.Event.ID = 1 # Bneg
+                        root.Event.RUN = i
+
+                        root.Event.V_out_hi[i][j] = float(devSERIAL.readline().decode('utf-8'))
+                        print(root.Event.V_out_hi[i][j])
+                        root.Event.V_gen_hi[i][j] = float(devSERIAL.readline().decode('utf-8'))
+                        print(root.Event.V_gen_hi[i][j])
+                        root.Event.V_out_lo[i][j] = float(devSERIAL.readline().decode('utf-8'))
+                        print(root.Event.V_out_lo[i][j])
+                        root.Event.V_gen_lo[i][j] = float(devSERIAL.readline().decode('utf-8'))
+                        print(root.Event.V_gen_lo[i][j])
+                    print(devSERIAL.readline().decode('utf-8'))
+                    _data0ttreeBneg.Fill()
+                    break;
+                break;
+    _file0.Write()
+    _file0.Close()
 
     return
+
+if __name__ == "__main__":
+    main()
